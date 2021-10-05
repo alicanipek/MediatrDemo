ec97da995a8e07a5ccd2d6761129563737023c5c
Ali Can İPEK <alican.ipek@outlook.com>
2021-10-05 19:53:06 +0300

    unit test project created

---
+++ tests/MediatrDemo.Library.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace MediatrDemo.Library.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
